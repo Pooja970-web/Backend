e7334525fd4d01154478f575ab614fb7d037ecf2
Pooja970-web <poojklh15@gmail.com>
2026-03-25 22:35:32 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/twitter',(req,res)=>{res.send('This is Twitter')})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const catme=require('cat-me');
+console.log(catme());
