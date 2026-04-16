b65c9bdf620d474cee7c1aa816722fd9de0add12
Pooja970-web <poojklh15@gmail.com>
2026-04-16 22:46:46 +0530

    .

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 const express=require('express');
-
+const noteModel = require("./models/note.model");
 const app=express();
 module.exports=app;
--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,7 @@
 const mongoose =require("mongoose");
 
 async function connectDB(){
-  await mongoose.connect(-process.env.MONGO_URL);
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("Connected to db")
 }
+module.exports=connectDB
